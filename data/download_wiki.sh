7feb46b3cc61ac1417416cc8ca515a34a4269454
cyx0406 <yuxiangchen2004@qq.com>
2024-12-03 03:23:16 +0000

    init transformers

--- data/download_wiki.sh
+++ data/download_wiki.sh
@@ -1,1 +1,1 @@
-wget https://huggingface.co/datasets/princeton-nlp/datasets-for-simcse/resolve/main/wiki1m_for_simcse.txt
+wget https://hf-mirror.com/datasets/princeton-nlp/datasets-for-simcse/resolve/main/wiki1m_for_simcse.txt
